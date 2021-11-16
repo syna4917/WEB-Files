ee35f049d636831da15605d051726f0d56a63d6b
syna4917 <70238690+syna4917@users.noreply.github.com>
2021-11-16 10:50:30 -0700

    week 11 actually

--- wk 11 actually/index.js
+++ wk 11 actually/index.js
@@ -5,5 +5,23 @@ let pic;
 
 for(i=0;i<pics;i++){
   pic = 1+i;
-document.getElementById('portfolio').innerHTML += "<img src='assets/portfolio/" + pic + ".jpg'" + "class='portfoliopic' id='pic" + pic+ "' />";
+document.getElementById('portfolio').innerHTML += "<div class = "pimaage"><img src='assets/portfolio/" + pic + ".jpg'" + "class='portfoliopic' id='pic" + pic+ "' /></div>";
+}
+
+let displaydetails = false:
+// just keep track of if variable is there or //
+document.getElementById('portforlio').addEventListener("click", showDetails);
+
+
+function showDetails(){
+  document.getElementById('details').style.display = "block";
+  document.getElementById('detailsoverlay').style.display = "block";
+  document.getElementById('detailsoverlay').addEventListener('click', clickedOverlay);
+  displaydetails = true;
+}
+
+function clickedOverlay(){
+  document.getElementById('details').style.display = "none";
+  document.getElementById('detailsoverlay').style.display = "none";
+  displaydetails = false;
 }
